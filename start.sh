c6b216a5d21f57c8fd15b3ba1efd5ba18bba6568
smalli <809885126@qq.com>
2025-02-10 09:20:07 +0800

    修改启动脚本

--- start.sh
+++ start.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-python launch.py --precision full --no-half --xformers --listen --enable-insecure-extension-access #
+python launch.py --xformers --listen --port 7860 \
+--models-dir ~/SoftWare/Sd/models/ \
+--enable-insecure-extension-access #--precision full --no-half
+
+#--ckpt-dir ~/SoftWare/Sd/models/Stable-diffusion \
+#--gfpgan-dir ~/SoftWare/Sd/models/GFPGAN \
+#--clip-models-path ~/SoftWare/Sd/models/openai \
